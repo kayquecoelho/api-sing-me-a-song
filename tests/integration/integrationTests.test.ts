64d1dccb0ed06dbf5211f7738902c0d723c618c6
Kayque <lkcoelho76@gmail.com>
2022-05-06 14:15:37 -0300

    feat: implement tests in post/recommendations/:id/upvote route

--- tests/integration/integrationTests.test.ts
+++ tests/integration/integrationTests.test.ts
@@ -24,6 +24,26 @@ describe("Integration Tests", () => {
     });
   });
 
+  describe("POST /recommendations/:id/upvote", () => {
+    beforeEach(truncateRecommendations);
+    afterAll(disconnect);
+
+    it("should return status 200 and increase the recommendation score", async () => {
+      const createdRecommendation = await recommendationFactory();
+
+      const response = await supertest(app).post(
+        `/recommendations/${createdRecommendation.id}/upvote`
+      );
+
+      const recommendation = await prisma.recommendation.findUnique({
+        where: { name: createdRecommendation.name },
+      });
+
+      expect(createdRecommendation.score + 1).toEqual(recommendation.score);
+      expect(response.status).toEqual(200);
+    });
+  });
+
 });
 
 async function truncateRecommendations() {
@@ -40,4 +60,24 @@ function recommendationBodyFactory() {
     youtubeLink: "https://www.youtube.com/watch?v=KsZ6tROaVOQ",
   };
   return recommendation;
+}
+
+async function recommendationFactory() {
+  const recommendation = recommendationBodyFactory();
+
+  const createdRecommendation = await prisma.recommendation.create({
+    data: recommendation,
+  });
+
+  return createdRecommendation;
+}
+
+async function lowScoreRecommendationFactory() {
+  const recommendation = recommendationBodyFactory();
+
+  const createdRecommendation = await prisma.recommendation.create({
+    data: { ...recommendation, score: -5 },
+  });
+
+  return createdRecommendation;
 }
