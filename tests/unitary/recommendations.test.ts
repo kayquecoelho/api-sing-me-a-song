279c6056fc1fbc09c1f53b40c60776e419350072
Kayque <lkcoelho76@gmail.com>
2022-05-07 15:30:01 -0300

    feat: implement unitary tests to getRandom service

--- tests/unitary/recommendations.test.ts
+++ tests/unitary/recommendations.test.ts
@@ -1,38 +1,44 @@
 import { jest } from "@jest/globals";
+import { Recommendation } from "@prisma/client";
 import { recommendationRepository } from "../../src/repositories/recommendationRepository.js";
 import { recommendationService } from "../../src/services/recommendationsService.js";
 import { conflictError, notFoundError } from "../../src/utils/errorUtils.js";
 import recommendationBodyFactory from "../factories/recommendationBodyFactory.js";
 
 describe("Unitary Tests Recommendations", () => {
-
   describe("POST /recommendations", () => {
     it("should return status 409 given a duplicate recommendation", async () => {
       const recommendation = recommendationBodyFactory();
-  
+
       jest
         .spyOn(recommendationRepository, "findByName")
         .mockResolvedValueOnce({ ...recommendation, score: 1, id: 1 });
-  
+
       expect(recommendationService.insert(recommendation)).rejects.toEqual(
         conflictError("Recommendation exists")
       );
     });
   });
-  
+
   describe("POST /recommendations/:id/upvote", () => {
     it("should return status 404 given an invalid recommendationId", async () => {
-      jest.spyOn(recommendationRepository, "findById").mockResolvedValueOnce(null);
-  
+      jest
+        .spyOn(recommendationRepository, "findById")
+        .mockResolvedValueOnce(null);
+
       expect(recommendationService.upvote(1)).rejects.toEqual(notFoundError());
     });
   });
-  
+
   describe("POST /recommendations/:id/downvote", () => {
     it("should return status 404 given an invalid recommendationId", async () => {
-      jest.spyOn(recommendationRepository, "findById").mockResolvedValueOnce(null);
-  
-      expect(recommendationService.downvote(1)).rejects.toEqual(notFoundError());
+      jest
+        .spyOn(recommendationRepository, "findById")
+        .mockResolvedValueOnce(null);
+
+      expect(recommendationService.downvote(1)).rejects.toEqual(
+        notFoundError()
+      );
     });
   });
 
@@ -44,12 +50,14 @@ describe("Unitary Tests Recommendations", () => {
     it("should throw a notFoundError given that there's no recommendations", async () => {
       jest.spyOn(global.Math, "random").mockReturnValueOnce(0.1);
 
-      const findAll = jest.spyOn(recommendationRepository, "findAll").mockResolvedValue([]);
-      
+      const findAll = jest
+        .spyOn(recommendationRepository, "findAll")
+        .mockResolvedValue([]);
+
       let response;
 
       try {
-        response = await recommendationService.getRandom()
+        response = await recommendationService.getRandom();
       } catch (error) {
         response = error;
       }
@@ -62,12 +70,14 @@ describe("Unitary Tests Recommendations", () => {
     it("should throw a notFoundError given that there's no recommendations", async () => {
       jest.spyOn(global.Math, "random").mockReturnValueOnce(0.8);
 
-      const findAll = jest.spyOn(recommendationRepository, "findAll").mockResolvedValue([]);
-      
+      const findAll = jest
+        .spyOn(recommendationRepository, "findAll")
+        .mockResolvedValue([]);
+
       let response;
 
       try {
-        response = await recommendationService.getRandom()
+        response = await recommendationService.getRandom();
       } catch (error) {
         response = error;
       }
@@ -75,6 +85,67 @@ describe("Unitary Tests Recommendations", () => {
       expect(response).toEqual(notFoundError());
       expect(findAll).toBeCalledWith({ score: 10, scoreFilter: "lte" });
       expect(findAll).toBeCalledWith();
+    });
+
+    it("should return a recommendation with score greater than 10", async () => {
+      const recommendations: Recommendation[] = [
+        {
+          id: 1,
+          name: "cardigan",
+          youtubeLink: "https://www.youtube.com/watch?v=K-a8s8OLBSE",
+          score: 15,
+        },
+        {
+          id: 2,
+          name: "august",
+          youtubeLink: "https://www.youtube.com/watch?v=nn_0zPAfyo8",
+          score: 14,
+        },
+      ];
+
+      jest.spyOn(global.Math, "random").mockReturnValue(0.5);
+
+      const findAll = jest
+        .spyOn(recommendationRepository, "findAll")
+        .mockResolvedValue(recommendations);
+
+      const response = await recommendationService.getRandom();
+
+      expect(findAll).toBeCalledWith({ score: 10, scoreFilter: "gt" });
+      expect(response).toEqual(recommendations[1]);
+    });
+
+    it("should return a recommendation with score between -5 and 10", async () => {
+      const recommendations: Recommendation[] = [
+        {
+          id: 1,
+          name: "cardigan",
+          youtubeLink: "https://www.youtube.com/watch?v=K-a8s8OLBSE",
+          score: 7,
+        },
+        {
+          id: 2,
+          name: "august",
+          youtubeLink: "https://www.youtube.com/watch?v=nn_0zPAfyo8",
+          score: 8,
+        },
+      ];
+
+      const random = jest
+        .spyOn(global.Math, "random")
+        .mockReturnValueOnce(0.9)
+        .mockReturnValueOnce(0.3);
+
+      const findAll = jest
+        .spyOn(recommendationRepository, "findAll")
+        .mockResolvedValue(recommendations);
+
+      const response = await recommendationService.getRandom();
+
+      expect(findAll).toBeCalledWith({ score: 10, scoreFilter: "lte" });
+      expect(random).toHaveNthReturnedWith(1, 0.9);
+      expect(random).toHaveNthReturnedWith(2, 0.3);
+      expect(response).toEqual(recommendations[0]);
     });
   });
 });
