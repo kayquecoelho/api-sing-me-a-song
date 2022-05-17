bf09e9e4368ca9183311c3951374fb611d24fe7f
Celso <celsolnv27@gmail.com>
2022-05-16 23:58:08 -0300

    Preparando revisão de código

--- tests/factories/recommendationBodyFactory.ts
+++
@@ -1,9 +0,0 @@
-import { CreateRecommendationData } from "../../src/services/recommendationsService";
-
-export default function recommendationBodyFactory() {
-  const recommendation: CreateRecommendationData = {
-    name: "the 1",
-    youtubeLink: "https://www.youtube.com/watch?v=KsZ6tROaVOQ",
-  };
-  return recommendation;
-}
